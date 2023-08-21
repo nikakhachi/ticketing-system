09e6ee8c9a997028d04830e7d3b5e066e2116453
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-08-21 17:36:05 +0400

    update solc version to 0.8.19

--- src/EventFactory.sol
+++ src/EventFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
 import "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
 import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
